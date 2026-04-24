84b00f3533c50c2295430a3fe6e2affa45ba4571
evilk1aus <124642134+evilk1aus@users.noreply.github.com>
2026-04-24 15:22:08 +0800

    #60 Fix menu bar logic to allow user to go back to main settings screen by pressing settings icon

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -26,9 +26,7 @@ class _SettingsPageState extends State<SettingsPage> {
   int _currentIndex = -1;
 
   void onBack() {
-    setState(() {
-      _currentIndex = -1;
-    });
+    Navigator.pop(context);
   }
 
   late final List<Widget> _pages = [
@@ -73,37 +71,40 @@ class _SettingsPageState extends State<SettingsPage> {
               text: "Information",
               icon: Icons.info_outline,
               onPressed: () {
-                setState(() {
-                  _currentIndex = 0;
-                });
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => _pages[0]),
+                );
               },
             ),
             FullWidthIconButton(
               text: "Voice Settings",
               icon: Icons.spatial_audio_off_rounded,
               onPressed: () {
-                setState(() {
-                  _currentIndex = 1;
-                });
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => _pages[1]),
+                );
               },
             ),
             FullWidthIconButton(
               text: "Wellness Check",
               icon: Icons.mood_sharp,
               onPressed: () {
-                setState(() {
-                  _currentIndex = 2;
-                });
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => _pages[2]),
+                );
               },
             ),
             FullWidthIconButton(
               text: "View Information Placeholder",
               icon: Icons.info_outline,
               onPressed: () {
-                print("Button pressed!");
-                setState(() {
-                  _currentIndex = 3;
-                });
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => _pages[4]),
+                );
               },
             ),
           ],
